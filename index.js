5c7b8c29689aec28e972164724f87cdfa37dc687
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-04-17 12:31:28 +0200

    xo ok

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ let gController = null;
 (function () {
 	'use strict';
 	document.addEventListener('DOMContentLoaded', () => {
-		const ClassController = function (theNow) {
+		const ClassController = function (/* theNow */) {
 			const self = this;
 			self.util = {
 				assert(theAssertion) {
 					if (theAssertion !== true) {
-						alert('assertion failed');
+						alert('assertion failed'); // eslint-disable-line no-alert
 					}
 				},
 				keys: theObject => {
 					const ret = [];
 					for (const aProperty in theObject) {
-						if (theObject.hasOwnProperty(aProperty)) {
+						if (theObject.hasOwnProperty(aProperty)) { // eslint-disable-line no-prototype-builtins
 							ret.push(aProperty);
 						}
 					}
 					return ret;
 				},
-				createElement: function createElement(theTag, theProperties, theClassList) {
+				createElement(theTag, theProperties, theClassList) {
 					const ret = document.createElement(theTag);
 					if ((typeof undefined !== typeof theProperties) && (theProperties !== null)) {
 						this.keys(theProperties).forEach(theProperty => {
@@ -32,15 +32,15 @@ let gController = null;
 						});
 					}
 					return ret;
-				},
+				}
 			};
 
-			self.buttonDisable = function enableButton(theButton) {
+			self.buttonDisable = function (theButton) {
 				theButton.classList.remove('normal');
 				theButton.classList.add('busy');
 			};
 
-			self.buttonEnable = function enableButton(theButton) {
+			self.buttonEnable = function (theButton) {
 				theButton.classList.remove('busy');
 				theButton.classList.add('normal');
 			};
@@ -49,38 +49,38 @@ let gController = null;
 				enabled: true
 			};
 
-			self.show = function show() {
+			self.show = function () {
 				const aDOMContainer = document.getElementById('container');
-				const aDOMTestDateResult = self.util.createElement('div', {textContent: ''}, ['result']);
+				// Const aDOMTestDateResult = self.util.createElement('div', {textContent: ''}, ['result']);
 				const aDOMFooter = self.util.createElement('div', null, ['footer']);
 				const aDOMLink = self.util.createElement('div');
-				const aDOMHref = self.util.createElement('a', {href: 'https://github.com/isghe/satoshieuro-eurobitcoin_converter', textContent:'github'});
-				aDOMLink.appendChild (aDOMHref);
+				const aDOMHref = self.util.createElement('a', {href: 'https://github.com/isghe/satoshieuro-eurobitcoin_converter', textContent: 'github'});
+				aDOMLink.appendChild(aDOMHref);
 				const aDOMBitcoin = self.util.createElement('div', {textContent: '1ig1p1awfjS5bQxy2s33AD8sVspy4YFMD'}, ['bitcoin']);
-				const aDOMQR = self.util.createElement('img', {src: '1ig1p1awfjS5bQxy2s33AD8sVspy4YFMD.png', alt:'1ig1p1awfjS5bQxy2s33AD8sVspy4YFMD', width:'200', height:'200'});
-				[aDOMLink, aDOMQR, aDOMBitcoin].forEach (theDOM => {
-					aDOMFooter.appendChild (theDOM);
+				const aDOMQR = self.util.createElement('img', {src: '1ig1p1awfjS5bQxy2s33AD8sVspy4YFMD.png', alt: '1ig1p1awfjS5bQxy2s33AD8sVspy4YFMD', width: '200', height: '200'});
+				[aDOMLink, aDOMQR, aDOMBitcoin].forEach(theDOM => {
+					aDOMFooter.appendChild(theDOM);
 				});
 
-				const aDOMInputSatoshiEuro = self.util.createElement('input', {type: 'text', value:'', maxlength:30, size:30,}, ['input_text']);
+				const aDOMInputSatoshiEuro = self.util.createElement('input', {type: 'text', value: '', maxlength: 30, size: 30}, ['input_text']);
 
 				const aDOMWrapResult = self.util.createElement('div', null, ['wrap-result']);
-				const aDOMLabelResult = self.util.createElement('span', {textContent:'Result:'});
-				const aDOMValueResult = self.util.createElement('span',null, ['result']);
+				const aDOMLabelResult = self.util.createElement('span', {textContent: 'Result:'});
+				const aDOMValueResult = self.util.createElement('span', null, ['result']);
 
-				[aDOMLabelResult, aDOMValueResult].forEach (theDOM => {
-					aDOMWrapResult.appendChild (theDOM);
+				[aDOMLabelResult, aDOMValueResult].forEach(theDOM => {
+					aDOMWrapResult.appendChild(theDOM);
 				});
 
 				const aDOMButtonConvert = self.util.createElement('div', {
 					textContent: 'Convert',
-					onclick: function (theEvent) {
-						if (true === self.model.enabled){
+					onclick(/* theEvent */) {
+						if (self.model.enabled === true) {
 							self.model.enabled = false;
-							self.buttonDisable (aDOMButtonConvert);
-							aDOMValueResult.textContent = 100000000/aDOMInputSatoshiEuro.value;
-							setTimeout ( function (){
-								self.buttonEnable (aDOMButtonConvert);
+							self.buttonDisable(aDOMButtonConvert);
+							aDOMValueResult.textContent = 100000000 / aDOMInputSatoshiEuro.value;
+							setTimeout(() => {
+								self.buttonEnable(aDOMButtonConvert);
 								self.model.enabled = true;
 							}, 1000);
 						}
